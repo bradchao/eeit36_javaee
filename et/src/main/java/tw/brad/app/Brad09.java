cd50473ce038e8994d2320306989ac37bc0f7c50
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-11-19 11:29:43 +0800

    10191129

---
+++ et/src/main/java/tw/brad/app/Brad09.java
@@ -0,0 +1,32 @@
+package tw.brad.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Brad09")
+public class Brad09 extends HttpServlet {
+	private int i;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		
+		String name = req.getParameter("name");
+		
+		//----------------------------------
+		resp.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		
+		out.print("Hello, " + name);
+		
+		resp.flushBuffer();
+		
+	}
+
+}
